a0261f13036de6560ee221e94bce4726297e6cf7
Pratik Shah <hello@pratikshah.website>
2019-12-29 22:10:44 +0530

    build

---
+++ build/precache-manifest.9cc0cc5308419e11f1e4454e64cc5a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6a3209136f62054051adeb99f3a6a9",
+    "url": "/whatsapp-BigQuiz/index.html"
+  },
+  {
+    "revision": "ac93629145dfcd9bdedf",
+    "url": "/whatsapp-BigQuiz/static/js/2.c9ea8002.chunk.js"
+  },
+  {
+    "revision": "b0377fb4f9243b8cfc04",
+    "url": "/whatsapp-BigQuiz/static/js/main.8d228b09.chunk.js"
+  },
+  {
+    "revision": "7ff4a5c0ff34a045781a",
+    "url": "/whatsapp-BigQuiz/static/js/runtime~main.69fb3707.js"
+  },
+  {
+    "revision": "eb71b14209960e6ab7752661334099b8",
+    "url": "/whatsapp-BigQuiz/static/media/bg.eb71b142.png"
+  },
+  {
+    "revision": "570e52acff687d03ea7b8489664fad75",
+    "url": "/whatsapp-BigQuiz/static/media/whatsapp-chat-example.570e52ac.zip"
+  }
+]);
